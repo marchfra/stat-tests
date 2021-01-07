be09f9ac784d0d15fc7338364a7c3d7ecc3adc6b
Francesco Marchisotti <francescomarchisotti@Francescos-MBP.homenet.telecomitalia.it>
2021-01-07 19:05:24 +0100

    v0.0.3a1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="stat_tests-marchfra",
-    version="0.0.2.a1",
+    version="0.0.3.a1",
     author="marchfra",
     author_email="marchfra1@gmail.com",
     description="A package that performs statistical tests",
