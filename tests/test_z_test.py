98fc35a41aac7fd672a24d130de50ee9ceecbc2f
Francesco Marchisotti <francescomarchisotti@Francescos-MBP.homenet.telecomitalia.it>
2021-01-07 18:56:51 +0100

    Improved input check. Now returns bool.

--- tests/test_z_test.py
+++ tests/test_z_test.py
@@ -2,6 +2,7 @@ from stat_tests import z_test
 import uncertainties as u
 
 def test_z_test():
-	assert z_test.z_test(u.ufloat(0.1, 0.1)) == f'The input value 0.10+/-0.10 is compatible with 0 with a significance of 5.0%'
-	assert z_test.z_test(u.ufloat(1, 0.1)) == f'The input value 1.00+/-0.10 is not compatible with 0 with a significance of 5.0%'
+	assert z_test.z_test(u.ufloat(0.01, 0.1)) == True
+	assert z_test.z_test(u.ufloat(1, 0.1)) == False
+	assert z_test.z_test(u.ufloat(0.01, 0.1), 10) == ValueError
 	assert z_test.z_test('a', 0.05) == AssertionError
